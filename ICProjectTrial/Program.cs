3177e0aae2c5af890053f11cf475d9adb214b331
Ashamechery <ashamechery11@gmail.com>
2022-03-24 05:08:54 +1300

    create new material added

--- ICProjectTrial/Program.cs
+++ ICProjectTrial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 namespace ICProjectTrial
@@ -37,6 +38,53 @@ namespace ICProjectTrial
                 }
 
 
+            }
+            //Create time and material
+            //click on administration dropdown
+            IWebElement administrationDropDown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            administrationDropDown.Click();
+            //click on time and material from dropdown
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+            //click create new
+            IWebElement createNew= driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+            createNew.Click();
+            //select material from typecode dropdown
+            IWebElement typecodeDropdown= driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[2]/span"));
+            typecodeDropdown.Click();
+            IWebElement selectMaterial= driver.FindElement(By.XPath("//*[@id='TypeCode_option_selected']"));
+            //type coode
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("123");
+            //type description
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.SendKeys("hai");
+            //price
+            IWebElement pricePerunit = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+            pricePerunit.Click();
+            IWebElement priceBox = driver.FindElement(By.Id("Price"));
+            priceBox.SendKeys("12");
+            //click save button
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(1000);
+            //clickon gotolast page
+            IWebElement lastPage = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            lastPage.Click();
+            Thread.Sleep(1000);
+
+            // check if created data in table
+            IWebElement lastData = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+            {
+                if(lastData.Text=="123")
+                {
+                    Console.WriteLine("last data created pass");
+                }
+
+            else
+                {
+                    Console.WriteLine("last data created fail");
+                }
             }
         }
     }
